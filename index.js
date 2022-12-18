55132948453ecce5bc691f143d86df2f58757c55
Tamir <37386851+tamireinhorn@users.noreply.github.com>
2022-12-17 21:07:33 -0300

    Add body parser + post and all get urls

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
-express = require('express');
-app = express();
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
 PORT = 3000;
 
+envelopes = [];
+
+app.use('/envelopes', bodyParser.json({ type: 'application/json' }));
+
+const validateEnvelope = (req, res, next) => {
+    const envelope = req.body;
+    envelope.budget = Number(envelope.budget);
+    // Verify if envelope has all we need:
+    if (!envelope.title || !envelope.budget || isNaN(envelope.budget)){
+        res.status(400).send(`Invalid envelope. Envelopes should be a JSON with a title and a budget.`);
+
+    }
+    else {
+        
+        next();
+    }
+    
+};
+
 app.get('/', (req, res, next) => res.send('Hello world'));
+
+app.get('/envelopes', (req, res, next) => res.send(envelopes));
+
+app.post('/envelopes', validateEnvelope, (req, res, next) => 
+            {
+            res.status(201).send('Envelope added!');
+            envelopes.push(req.body)
+            }
+        );
 
 
 app.listen(PORT, () => 
