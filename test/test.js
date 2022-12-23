a625f184c04914486c804f8b19db14cb7955e7b8
Tamir <37386851+tamireinhorn@users.noreply.github.com>
2022-12-22 21:40:24 -0300

    Begin testing suite for #1

--- test/test.js
+++ test/test.js
@@ -3,3 +3,24 @@ const request = require('supertest');
 
 const app = require('../index');
 
+describe('envelope routes', function() {
+
+    describe('GET envelope', function() {
+
+
+        it('returns an array', function () {
+            return request(app)
+                .get('/envelopes')
+                .expect(200)
+                .then((response) => {expect(response.body).to.be.an.instanceOf(Array)});
+        });
+
+    });
+
+    describe('PUT envelope', function() {
+        
+    })
+
+
+
+});
